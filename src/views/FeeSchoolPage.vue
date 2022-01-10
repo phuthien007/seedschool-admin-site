d23d1e733db4624f780ea1d2addd09b1916ac7ce
phuthien007 <phailamsaonana@gmail.com>
2022-01-10 11:27:29 +0700

    ui fee st

--- src/views/FeeSchoolPage.vue
+++ src/views/FeeSchoolPage.vue
@@ -101,19 +101,64 @@
 
                         <v-tab-item>
                             <v-card flat>
-                                <v-card-text>
-                                    <p>
-                                        Sed aliquam ultrices mauris. Donec posuere vulputate arcu. Morbi ac felis. Etiam feugiat lorem non metus. Sed a libero.
-                                    </p>
 
-                                    <p>
-                                        Nam ipsum risus, rutrum vitae, vestibulum eu, molestie vel, lacus. Aenean tellus metus, bibendum sed, posuere ac, mattis non, nunc. Aliquam lobortis. Aliquam lobortis. Suspendisse non nisl sit amet velit hendrerit rutrum.
-                                    </p>
-
-                                    <p class="mb-0">
-                                        Phasellus dolor. Fusce neque. Fusce fermentum odio nec arcu. Pellentesque libero tortor, tincidunt et, tincidunt eget, semper nec, quam. Phasellus blandit leo ut odio.
-                                    </p>
+                                <v-card-text class="pt-4">
+                                    Thông tin học phí học sinh
                                 </v-card-text>
+
+                                <v-divider></v-divider>
+
+                                <v-virtual-scroll :items="items" :item-height="40" height="350">
+                                    <template v-slot:default="{ item }">
+                                        <v-list-item>
+                                            <v-list-item-avatar>
+                                                <v-avatar :color="item.color" size="56" class="white--text">
+                                                    <v-icon> mdi-account </v-icon>
+                                                </v-avatar>
+                                            </v-list-item-avatar>
+
+                                            <v-list-item-content>
+                                                <v-list-item-title>{{ item.fullName }}</v-list-item-title>
+                                            </v-list-item-content>
+                                            <v-list-item-content>
+                                                <v-list-item-title>Chưa nộp tiền</v-list-item-title>
+                                            </v-list-item-content>
+
+                                            <v-list-item-action>
+
+                                                <v-row justify="center">
+                                                    <v-dialog v-model="dialog" scrollable max-width="300px">
+                                                        <template v-slot:activator="{ on, attrs }">
+                                                            <v-btn depressed small v-bind="attrs" v-on="on">
+                                                                Xem chi tiết
+                                                                <v-icon color="orange darken-4" right>
+                                                                    mdi-open-in-new
+                                                                </v-icon>
+                                                            </v-btn>
+
+                                                        </template>
+                                                        <v-card>
+                                                            <v-divider></v-divider>
+                                                            <v-card-text style="height: 300px;">
+                                                                Các khoản thu phí
+                                                            </v-card-text>
+                                                            <v-divider></v-divider>
+                                                            <v-card-actions>
+                                                                <v-btn color="blue darken-1" text @click="dialog = false">
+                                                                    Đóng
+                                                                </v-btn>
+                                                                <v-btn color="blue darken-1" text @click="dialog = false">
+                                                                    Lưu
+                                                                </v-btn>
+                                                            </v-card-actions>
+                                                        </v-card>
+                                                    </v-dialog>
+                                                </v-row>
+
+                                            </v-list-item-action>
+                                        </v-list-item>
+                                    </template>
+                                </v-virtual-scroll>
                             </v-card>
                         </v-tab-item>
                         <v-tab-item>
@@ -227,7 +272,35 @@
 
 <script>
 export default {
-    name: "fee-school-page"
+    name: "fee-school-page",
+    data() {
+        return {
+            dialogm1: '',
+            dialog: false,
+            colors: ['#2196F3', '#90CAF9', '#64B5F6', '#42A5F5', '#1E88E5', '#1976D2', '#1565C0', '#0D47A1', '#82B1FF', '#448AFF', '#2979FF', '#2962FF'],
+            names: ['Oliver', 'Jake', 'Noah', 'James', 'Jack', 'Connor', 'Liam', 'John', 'Harry', 'Callum', 'Mason', 'Robert', 'Jacob', 'Jacob', 'Jacob', 'Michael', 'Charlie', 'Kyle', 'William', 'William', 'Thomas', 'Joe', 'Ethan', 'David', 'George', 'Reece', 'Michael', 'Richard', 'Oscar', 'Rhys', 'Alexander', 'Joseph', 'James', 'Charlie', 'James', 'Charles', 'William', 'Damian', 'Daniel', 'Thomas', 'Amelia', 'Margaret', 'Emma', 'Mary', 'Olivia', 'Samantha', 'Olivia', 'Patricia', 'Isla', 'Bethany'],
+            surnames: ['Smith', 'Anderson', 'Clark', 'Wright', 'Mitchell', 'Johnson', 'Thomas', 'Rodriguez', 'Lopez', 'Perez', 'Williams', 'Jackson', 'Lewis', 'Hill', 'Roberts', 'Jones', 'White', 'Lee', 'Scott', 'Turner', 'Brown', 'Harris', 'Walker', 'Green', 'Phillips', 'Davis', 'Martin', 'Hall', 'Adams', 'Campbell', 'Miller', 'Thompson', 'Allen', 'Baker', 'Parker', 'Wilson', 'Garcia', 'Young', 'Gonzalez', 'Evans', 'Moore', 'Martinez', 'Hernandez', 'Nelson', 'Edwards', 'Taylor', 'Robinson', 'King', 'Carter', 'Collins'],
+
+        }
+    },
+
+    computed: {
+        items() {
+
+            return Array.from({
+                length: 10000
+            }, () => {
+                const name = this.names[0]
+                const surname = this.surnames[1]
+
+                return {
+                    color: 'Oliver',
+                    fullName: `${name} ${surname}`,
+                    initials: `${name} ${surname}`,
+                }
+            })
+        },
+    }
 }
 </script>
 
